e6ce4f6578adfa9b000be69867cc9de8e228fa26
Akshay022 <akshaychauhan123.ac@gmail.com>
2025-04-01 18:37:37 +0530

    tag line change

--- app/page.jsx
+++ app/page.jsx
@@ -59,6 +59,7 @@ export default function Home() {
                   Exquisite wooden and metal decorative items, launching soon.
                 </p>
                 <p className="mb-2">Let's meet in the market.</p>
+                
 
                 <div className="mb-12">
                   <CountdownTimer targetDate={launchDate} />
